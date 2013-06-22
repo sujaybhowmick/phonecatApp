29023c9cb1629ef4a29ef25e6e0db7c3b461e17c
sujay <sujaybhowmick@gmail.com>
2013-06-22 22:02:18 +0530

    added phone data import groovy script and removed from bootstrap

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,33 +1,6 @@
-import com.phoneApp.app.Phone
-import com.phoneApp.app.PhoneDetail
-
-import groovy.json.JsonSlurper
-import groovy.io.FileType
-import org.codehaus.groovy.grails.commons.ApplicationHolder
-
 class BootStrap {
 
     def init = { servletContext ->
-       /* def slurper = new JsonSlurper()
-        FileReader reader = new FileReader("phones.json")
-        def phones = slurper.parse(reader)
-        def jsonFolder = 'phones-data'
-        def dir = ApplicationHolder.application.parentContext.getResource(jsonFolder).file
-        dir.traverse(
-                type:FileType.FILES,
-                nameFilter:~/.*\.json/) {
-            def details = slurper.parseText(it.text)
-            phones.each {jsonPhone ->
-                if(jsonPhone.id == details.id){
-                    def phone = new Phone(name:jsonPhone.name, age:jsonPhone.age,
-                                        snippet:jsonPhone.snippet, imageUrl:jsonPhone.imageUrl)
-                    def phoneDetail = new PhoneDetail(details: details)
-                    phone.phoneDetails = phoneDetail
-                    phone.save()
-                }
-            }
-        }
-        */
     }
 
     def destroy = {
---
+++ scripts/ImportPhoneData.groovy
@@ -0,0 +1,26 @@
+import com.phoneApp.app.Phone
+import com.phoneApp.app.PhoneDetail
+
+import groovy.json.JsonSlurper
+import groovy.io.FileType
+import org.codehaus.groovy.grails.commons.ApplicationHolder
+
+def slurper = new JsonSlurper()
+FileReader reader = new FileReader("phones.json")
+def phones = slurper.parse(reader)
+def jsonFolder = 'web-app/phones-data'
+def dir = new File(jsonFolder)
+dir.traverse(
+        type:FileType.FILES,
+        nameFilter:~/.*\.json/) {
+    def details = slurper.parseText(it.text)
+    phones.each {jsonPhone ->
+        if(jsonPhone.id == details.id){
+            def phone = new Phone(name:jsonPhone.name, age:jsonPhone.age,
+                                snippet:jsonPhone.snippet, imageUrl:jsonPhone.imageUrl)
+            def phoneDetail = new PhoneDetail(details: details)
+            phone.phoneDetails = phoneDetail
+            phone.save()
+        }
+    }
+}
